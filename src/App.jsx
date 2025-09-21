3e9d3fb9ba0c28923739c2dbb62c9c0b4b078390
Brando Arias <brariasm@desarrollo-ultrasist.com.mx>
2025-09-21 23:21:19 +0000

    Primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Brando Arias",
+  title: "Digital Craftsman",
+  email: "brariasm@desarrollo-ultrasist.com.mx",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
